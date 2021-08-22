4c155222852e3c8b3dcfc19efc7d9fccf93c8473
Rishi kesh <rishimkesh@gmail.com>
2021-08-23 05:20:20 +0530

    third

--- src/main/java/com/grv/NationalScholarshipPortalApplication.java
+++ src/main/java/com/grv/NationalScholarshipPortalApplication.java
@@ -10,6 +10,7 @@ public class NationalScholarshipPortalApplication {
 		SpringApplication.run(NationalScholarshipPortalApplication.class, args);
 		System.out.println("helllo");
 		// system
+		// my pro
 		
 	}
 
