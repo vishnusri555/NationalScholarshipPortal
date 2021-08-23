9e39089753e76622c00f3a9304beac7c9765e965
Rishi kesh <rishimkesh@gmail.com>
2021-08-23 06:14:35 +0530

    fourth

--- src/main/java/com/grv/NationalScholarshipPortalApplication.java
+++ src/main/java/com/grv/NationalScholarshipPortalApplication.java
@@ -9,13 +9,9 @@ public class NationalScholarshipPortalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(NationalScholarshipPortalApplication.class, args);
 		System.out.println("helllo");
-<<<<<<< HEAD
-		// system
-		// my pro
-=======
-		//vishnu change
+
 		System.out.println("change");
->>>>>>> 94817873af6bc635bf7d8f0b8359fb66dc9a76bd
+
 		
 	}
 
