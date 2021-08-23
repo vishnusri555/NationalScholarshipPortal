3de52bad269e391664ebe4890686544feb38e647
rishikesh-github <57387318+rishikesh-github@users.noreply.github.com>
2021-08-23 20:54:12 +0530

    Update NationalScholarshipPortalApplication.java

--- src/main/java/com/grv/NationalScholarshipPortalApplication.java
+++ src/main/java/com/grv/NationalScholarshipPortalApplication.java
@@ -8,7 +8,6 @@ public class NationalScholarshipPortalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NationalScholarshipPortalApplication.class, args);
-
 		System.out.println("helllo");
 
 		System.out.println("change");
