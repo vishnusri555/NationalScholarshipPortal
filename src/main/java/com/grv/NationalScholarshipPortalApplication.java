b1aaa3d372ad2311de2ede12ca8c61a4a2c0bd01
vishnusri555 <68071111+vishnusri555@users.noreply.github.com>
2021-08-24 10:37:42 +0530

    Update NationalScholarshipPortalApplication.java

--- src/main/java/com/grv/NationalScholarshipPortalApplication.java
+++ src/main/java/com/grv/NationalScholarshipPortalApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NationalScholarshipPortalApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NationalScholarshipPortalApplication.class, args);
 		System.out.println("helllo");
