0e884f009f6ea15982bc08e1915a31d8967cae4e
rishikesh-github <57387318+rishikesh-github@users.noreply.github.com>
2021-08-24 06:00:14 +0530

    educationaldetails and famildetails and studentdocuments

---
+++ src/main/java/com/grv/model/EducationDetails.java
@@ -0,0 +1,172 @@
+package com.grv.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="education_details")
+public class EducationDetails {
+	
+	@Id
+	@Column(name="student_id")
+	private int studentId;
+
+	@Column(name="sslc_rollno")
+	private String sslcRollNo;
+	
+	@Column(name="hsc_rollno")
+	private String hscRollNo;
+	
+	@Column(name="sslc_board_name")
+	private String sslcBoardName;
+	
+	@Column(name="sslc_passing_year")
+	private Date sslcPassingYear;
+	
+	@Column(name="hsc_passing_year")
+	private Date hscPassingYear;
+	
+	@Column(name="sslc_percentage")
+	private int sslcPercentage;
+	
+	@Column(name="hsc_percentage")
+	private int hscPercentage;
+	
+	@Column(name="institute_name")
+	private String instituteName;
+	
+	@Column(name="present_year")
+	private Date presentYear;
+	
+	@Column(name="present_course")
+	private String presentCourse;
+	
+	@Column(name="university_name")
+	private String universityName;
+	
+	@Column(name="class_start_date")
+	private Date classStartDate;
+	
+	@Column(name="passing_year")
+	private Date passingYear;
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getSslcRollNo() {
+		return sslcRollNo;
+	}
+
+	public void setSslcRollNo(String sslcRollNo) {
+		this.sslcRollNo = sslcRollNo;
+	}
+
+	public String getHscRollNo() {
+		return hscRollNo;
+	}
+
+	public void setHscRollNo(String hscRollNo) {
+		this.hscRollNo = hscRollNo;
+	}
+
+	public String getSslcBoardName() {
+		return sslcBoardName;
+	}
+
+	public void setSslcBoardName(String sslcBoardName) {
+		this.sslcBoardName = sslcBoardName;
+	}
+
+	public Date getSslcPassingYear() {
+		return sslcPassingYear;
+	}
+
+	public void setSslcPassingYear(Date sslcPassingYear) {
+		this.sslcPassingYear = sslcPassingYear;
+	}
+
+	public Date getHscPassingYear() {
+		return hscPassingYear;
+	}
+
+	public void setHscPassingYear(Date hscPassingYear) {
+		this.hscPassingYear = hscPassingYear;
+	}
+
+	public int getSslcPercentage() {
+		return sslcPercentage;
+	}
+
+	public void setSslcPercentage(int sslcPercentage) {
+		this.sslcPercentage = sslcPercentage;
+	}
+
+	public int getHscPercentage() {
+		return hscPercentage;
+	}
+
+	public void setHscPercentage(int hscPercentage) {
+		this.hscPercentage = hscPercentage;
+	}
+
+	public String getInstituteName() {
+		return instituteName;
+	}
+
+	public void setInstituteName(String instituteName) {
+		this.instituteName = instituteName;
+	}
+
+	public Date getPresentYear() {
+		return presentYear;
+	}
+
+	public void setPresentYear(Date presentYear) {
+		this.presentYear = presentYear;
+	}
+
+	public String getPresentCourse() {
+		return presentCourse;
+	}
+
+	public void setPresentCourse(String presentCourse) {
+		this.presentCourse = presentCourse;
+	}
+
+	public String getUniversityName() {
+		return universityName;
+	}
+
+	public void setUniversityName(String universityName) {
+		this.universityName = universityName;
+	}
+
+	public Date getClassStartDate() {
+		return classStartDate;
+	}
+
+	public void setClassStartDate(Date classStartDate) {
+		this.classStartDate = classStartDate;
+	}
+
+	public Date getPassingYear() {
+		return passingYear;
+	}
+
+	public void setPassingYear(Date passingYear) {
+		this.passingYear = passingYear;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/grv/model/FamilyDetails.java
@@ -0,0 +1,104 @@
+package com.grv.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="family_details")
+public class FamilyName {
+
+	@Id
+	@Column(name="student_id")
+	private int studentId;
+	
+	@Column(name="father_name")
+	private String fatherName;
+	
+	@Column(name="mother_name")
+	private String motherName;
+	
+	@Column(name="annual_income")
+	private double annualIncome;
+	
+	@Column(name="religion")
+	private String religion;
+	
+	@Column(name="aadhar_no")
+	private String aadharNo;
+	
+	@Column(name="type_of_disability")
+	private boolean typeOfDisability;
+	
+	@Column(name="marital_status")
+	private String maritalStatus;
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getFatherName() {
+		return fatherName;
+	}
+
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+
+	public String getMotherName() {
+		return motherName;
+	}
+
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+
+	public double getAnnualIncome() {
+		return annualIncome;
+	}
+
+	public void setAnnualIncome(double annualIncome) {
+		this.annualIncome = annualIncome;
+	}
+
+	public String getReligion() {
+		return religion;
+	}
+
+	public void setReligion(String religion) {
+		this.religion = religion;
+	}
+
+	public String getAadharNo() {
+		return aadharNo;
+	}
+
+	public void setAadharNo(String aadharNo) {
+		this.aadharNo = aadharNo;
+	}
+
+	public boolean isTypeOfDisability() {
+		return typeOfDisability;
+	}
+
+	public void setTypeOfDisability(boolean typeOfDisability) {
+		this.typeOfDisability = typeOfDisability;
+	}
+
+	public String getMaritalStatus() {
+		return maritalStatus;
+	}
+
+	public void setMaritalStatus(String maritalStatus) {
+		this.maritalStatus = maritalStatus;
+	}
+	
+	
+	
+	
+}
