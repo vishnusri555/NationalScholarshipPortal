ff78ca41fc9639b5f5fd27ad0000fd20d234dc57
vishnu <vishnu@gmail.com>
2021-08-24 22:06:02 +0530

    Crud operations completed

--- src/main/java/com/grv/dao/CrudDao.java
+++ src/main/java/com/grv/dao/CrudDao.java
@@ -1,12 +1,13 @@
 package com.grv.dao;
 
 import java.util.List;
-import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.grv.model.Institution;
 import com.grv.model.Schemes;
 import com.grv.model.Student;
+import com.grv.repository.InstitutionRepo;
 import com.grv.repository.SchemesRepo;
 import com.grv.repository.StudentRepo;
 
@@ -19,12 +20,16 @@ public class CrudDao {
 	@Autowired
 	SchemesRepo schemesRepo;
 	
+	@Autowired
+	InstitutionRepo institutionRepo;
+	
+	//Student CRUD
 	public List<Student> getAllStudents() {
 		return studentRepo.findAll();
 	}
 	
 	public Student getStudent(int studentId) {
-		return studentRepo.getById(studentId);
+		return studentRepo.getById(studentId) != null?studentRepo.getById(studentId):null;	
 	}
 	
 	public Student addStudent(Student student) {
@@ -35,10 +40,18 @@ public class CrudDao {
 		return studentRepo.save(student);
 	}
 	
-	public void deleteStudent(Student student) {
-		studentRepo.delete(student);
+	public boolean deleteStudent(Student student) {
+		int id = student.getStudentId();
+		if(studentRepo.getById(id)!=null) {
+			studentRepo.delete(student);
+			return true;
+		}
+		else 
+			return false;
+		
 	}
 	
+	//Schemes CRUD
 	public List<Schemes> getAllSchemes() {
 		return schemesRepo.findAll();
 	}
@@ -55,7 +68,40 @@ public class CrudDao {
 		return schemesRepo.save(scheme);
 	}
 	
-//	public Schemes deleteScheme(Scheme scheme) {
-//		
-//	}
+	public boolean deleteScheme(Schemes scheme) {
+		int id = scheme.getSchemeId();
+		if(schemesRepo.getById(id)!=null ) {
+			schemesRepo.delete(scheme);
+			return true;
+		}
+		else return false;		
+	}
+	
+	//Institution CRUD
+	public List<Institution> getAllInstitutions() {
+		return institutionRepo.findAll();
+	}
+	
+	public Institution getInstitution(String institutionCode) {
+		return institutionRepo.getById(institutionCode);
+	}
+	
+	public boolean addInstitution(Institution institution) {
+		return institutionRepo.save(institution) != null;
+	}
+	
+	public boolean updateInstitution(Institution institution) {
+		return institutionRepo.save(institution) != null;
+	}
+	
+	public boolean deleteInstitution(Institution institution) {
+		String id = institution.getInstitutionCode();
+		if(institutionRepo.getById(id) != null) 
+		{
+			institutionRepo.delete(institution);
+			return true;
+		}
+		else 
+			return false;
+	}
 }
--- src/main/java/com/grv/service/CrudService.java
+++ src/main/java/com/grv/service/CrudService.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.grv.dao.CrudDao;
+import com.grv.model.Institution;
+import com.grv.model.Schemes;
 import com.grv.model.Student;
 
 @Service
@@ -16,6 +18,7 @@ public class CrudService {
 	@Autowired
 	CrudDao crudDao;
 	
+	//student CRUD
 	public List<Student> getAllStudents() {
 		return crudDao.getAllStudents();
 	}
@@ -30,5 +33,51 @@ public class CrudService {
 	
 	public Student updateStudent(Student student) {
 		return crudDao.updateStudent(student);
+	}
+	
+	public boolean deleteStudent(Student student) {
+		return crudDao.deleteStudent(student);
+	}
+	
+	//Scheme CRUD
+	public List<Schemes> getAllSchemes() {
+		return crudDao.getAllSchemes();
+	}
+	
+	public Schemes getScheme(int schemeId) {
+		return crudDao.getSchemes(schemeId);
+	}
+	
+	public boolean addScheme(Schemes scheme) {
+		return crudDao.addScheme(scheme) != null;
+	}
+	
+	public boolean updateScheme(Schemes scheme) {
+		return crudDao.updateScheme(scheme) != null;
+	}
+	
+	public boolean deleteSchemes(Schemes scheme) {
+		return crudDao.deleteScheme(scheme);
+	}
+	
+	//Institution CRUD
+	public List<Institution> getAllInstitutions() {
+		return crudDao.getAllInstitutions();
+	}
+	
+	public Institution getInstitution(String institutionCode) {
+		return crudDao.getInstitution(institutionCode);
+	}
+	
+	public boolean addInstitution(Institution institution) {
+		return crudDao.addInstitution(institution);
+	}
+	
+	public boolean updateInstitution(Institution institution) {
+		return crudDao.updateInstitution(institution);
+	}
+	
+	public boolean deleteInstitution(Institution institution) {
+		return crudDao.deleteInstitution(institution);
 	}
 }
